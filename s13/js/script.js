f36e3862756f80b4b6503e7755a0e78063e277bd
NouranFayez <nouran_ahmed269@hotmail.com>
2025-11-11 17:21:16 +0200

    session13

---
+++ s13/js/script.js
@@ -0,0 +1,103 @@
+console.log("javascript")
+
+// var let const
+
+var firstName = "Nouran"
+
+var age = 31
+
+var lastName = "Fayez"
+
+var fullName = firstName + " " + lastName
+// template string
+var fullName = `${firstName} ${lastName}`
+
+
+var isGraduated = true
+
+var address 
+
+// console.log(h)
+
+var h 
+// console.log(fullName)
+
+var k = null
+// console.log(k)
+
+// var userName = prompt("enter your name" , "Ahmed")
+
+// console.log(userName)
+
+
+// Mathematical operators ==> + - / * ** % ++ --
+
+var k = 10
+var l = 20
+// console.log(k + l)
+
+// console.log(11%2)
+
+// ++ increment
+
+var a = 10
+
+// console.log(a++) 
+// console.log(++a)
+// console.log(a)
+ // -- decrement
+
+//  console.log(--a)
+//  console.log(a--)
+//  console.log(a)
+
+//  comparison operators ==> > >= < <= == === != !==
+
+var g = 20
+
+var h = 30
+// console.log(g > h)
+// console.log(g >= k)
+
+
+var l = 20
+var k = "20"
+
+// console.log(l == k) // value
+// console.log(l === k) // value - data type
+
+// console.log(k != l) // value
+// console.log(k !== l) // value - data type
+
+// console.log(typeof(l))
+// console.log(typeof k )
+// console.log(typeof k == "String");
+
+
+
+var userName = "Marwa"
+var userAddress = "October"
+
+// console.log(userName == "Marwa" && userAddress == "Maadi"  )
+// console.log(userName == "Marwa" || userAddress == "Maadi"  )
+// // console.log(userAddress == "Maadi")
+
+// console.log(!(userAddress == "Nasr city"))
+
+//   ternary operator ==> (condition) ? true : false
+
+// var p = prompt('enter your job');
+// (p == "developer" || p == "Developer") ? console.log('valid user') : console.log('invalid user');
+
+
+
+// false and truthy values
+// var m = 0; // false
+// var m = 10; // true
+// var m = "text"; // true
+// var m = ""; // false
+// var m = null; // false
+// var m 
+// var m = -10; // true
+
+// (m) ? console.log('correct') : console.log('wrong')
